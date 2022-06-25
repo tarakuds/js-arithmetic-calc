846ae737424c2f1de5931c55ae5174f13472457e
tarakuds <taradebby@gmail.com>
2022-06-25 11:26:37 +0100

    arithmetic operators

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(x,y){
+    return x+y
+}
+
+function sub(x,y){
+    return (x-y)
+}
+
+function div(x,y){
+    return x/y
+}
+
+function mul(x,y){
+    return x*y
+}
+
+console.log(add(2,3))
+
+console.log(sub(2,3))
+
+console.log(mul(2,3))
+
+console.log(div(2,3))
